5c9131be313fcd648bd0acb5af5ebf9d900f2059
morland <mqy_bs@hotmail.com>
2017-10-29 14:02:32 +0800

    background start

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import gevent
+from threading import Thread
 from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
 from geventwebsocket._compat import range_type
 from controller import SimpleSwitch
@@ -8,8 +9,8 @@ class WSGI_SERVER(SimpleSwitch):
     def __init__(self, *args, **kwargs):
         super(WSGI_SERVER, self).__init__(*args, **kwargs)
         self.server = WebSocketServer(
-            ('', 8080), resource, debug=True, _logger=self.logger)
-        self.server.serve_forever()
+            ('', 8080), self.resource, debug=True, _logger=self.logger)
+        Thread(target=self.server.serve_forever(), args=(10,))
 
 
 class TestApplication(WebSocketApplication):
@@ -20,12 +21,12 @@ class TestApplication(WebSocketApplication):
         print("Connection Closed!!!", reason)
 
 
-def static_wsgi_app(environ, start_response):
+def static_wsgi_app(self, environ, start_response):
     start_response("200 OK", [("Content-Type", "text/html")])
     return "hello world"
 
 
 resource = Resource([
-    ('/', static_wsgi_app),
-    ('/data', TestApplication)
+    ('/', self.static_wsgi_app),
+    ('/data', self.TestApplication)
 ])
